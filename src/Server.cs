aa209c343feee74b3f9bd43b818e131fa901afb6
Surya Suresh <Surya.Suresh@epiplex.ai>
2026-08-24 18:33:26 +0530

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -5,6 +5,6 @@ using System.Net.Sockets;
 Console.WriteLine("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
-// TcpListener server = new TcpListener(IPAddress.Any, 4221);
-// server.Start();
-// server.AcceptSocket(); // wait for client
+TcpListener server = new TcpListener(IPAddress.Any, 4221);
+server.Start();
+server.AcceptSocket(); // wait for client
